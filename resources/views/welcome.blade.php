25a9d93f166615e1e27f4907c2e6ade32a6c736b
Nur Mohammad <nur.mohd1996@gmail.com>
2021-07-18 00:37:50 +0600

    vue js intigrate in laravel, crud operation using vue js

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+    <h1 class="text-white display-5">Vue js laravel todo list</h1>
+    <todo-component></todo-component>
+</div>
+  
+@endsection
